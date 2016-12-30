b5d5dee42c3cfd1ae1d8778892ee405f4d59ebdb
Rk <rkramesh@users.noreply.github.com>
2016-12-30 15:48:53 +0530

    changes

--- Cricbuzz/Score-scrapper.py
+++ Cricbuzz/Score-scrapper.py
@@ -1,12 +1,13 @@
 import os, re
 import bs4,time
 import requests
-class Score(object):
 
+
+class Score(object):
     def __init__(self):
+
         #will be using it for enhancements
         pass
-   
     @staticmethod
     def getMatch():
         url='http://www.cricbuzz.com/api/html/matches-menu'
@@ -17,20 +18,26 @@ class Score(object):
                                                        'Gecko) Chrome/37.0.2062.'
                                                        '120 Safari/537.36'})
         soup = bs4.BeautifulSoup(response.content, "html.parser")
-        optionlist={}#
+        optionlist={}
+        status={}#
+        global mstatus
         for count,tag in enumerate (soup.find_all('li',{'class':re.compile('cb-lst-mtch')})):
             print ("Enter '{}' for '{}' ".format(count,tag.a['title']))
             optionlist[count] = tag.a['href']
+            status[count]=tag.a['title']
         choice = int(raw_input("\n>Enter Your Option here: "))
         if choice in optionlist:
-           return Score.getScore('http://www.cricbuzz.com'+optionlist[choice])
+           mstatus=status[choice]
+           return Score.getScore('http://www.cricbuzz.com'+optionlist[choice],mstatus)
+           
         else:
             print'Wrong Option,Select again'
             Score.getMatch()
     @staticmethod
-    def getScore(url):
+    def getScore(url,mstatus):
+       
         while True:
-            
+##            print test.rsplit(None, 1)[-1]
             response = requests.get(url,
                                     headers={'User-agent': 'Mozilla/5.0 (Windows NT '
                                                            '6.2; WOW64) AppleWebKit/'
@@ -40,30 +47,42 @@ class Score(object):
             soup = bs4.BeautifulSoup(response.content, "html.parser")
             
             print '\n'+soup.title.text+'\n'
-
-            if soup.find('div',{'class':re.compile('cb-mini-col')}) :
+            
+            if mstatus.rsplit(None, 1)[-1] == 'Live' :
                 print soup.find('div',{'class':re.compile('cb-mini-col')} ).text.strip()+'\n'
                 match='live'
             else:
                 match='past'
             duplicate=''#remove duplicate entries
+            
             for tag in soup.find_all(re.compile('.')):
                 try:
                     if match == 'live':
-                        print tag.div.find('div',{'class':re.compile('.*cb-ovr-num')}).text+' '+tag.p.text
+                         print tag.div.find('div',{'class':re.compile('.*cb-ovr-num')}).text+' '+tag.p.text
                     elif match == 'past':
                         if len(tag.p.text) == duplicate:
                             continue
                         else:
-                            print tag.p.text+'\n'
-                            duplicate=len(tag.p.text)
+                            if tag.div.find('div',{'class':re.compile('.*cb-ovr-num')}):
+                                print tag.div.find('div',{'class':re.compile('.*cb-ovr-num')}).text+' '+tag.p.text
+                            else:
+                                print tag.p.text+'\n'
+                                duplicate=len(tag.p.text)
                 except:
                     pass
             if match == 'past':
                 break
+           
             else:
                 time.sleep(12)
-Score.getMatch()
+
+
+while True:
+    print "\n" * 500
+    Score.getMatch()
+    print '#'*30+mstatus+'#'*30
+    raw_input("Press Any Key Continue  or CTRL+C to exit!...")
+    
     
 
     
